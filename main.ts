40333bfb729f34e498225c3ceca736f184c08f7f
jkbgu <95989717+jkbgu@users.noreply.github.com>
2021-12-12 10:02:31 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let _0 = 0
+let _2 = 0
 basic.forever(function () {
     led.plotBrightness(randint(0, 4), randint(0, 4), randint(0, 225))
+    _0 = led.pointBrightness(0, 0)
+    music.setVolume(input.lightLevel())
+    music.playMelody("F D A C B C C5 E ", 180)
+    _2 = input.magneticForce(Dimension.X)
 })
